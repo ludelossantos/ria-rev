2637915d5c6f2e6c2ea35dfc12dac85302a749dd
silBH <berrueta.hermida@gmail.com>
2023-06-06 22:02:54 -0300

    estilos arreglados

--- src/paginas/InfoLibro.js
+++ src/paginas/InfoLibro.js
@@ -27,28 +27,28 @@ const InfoLibro = () => {
         <div>
             <Header />
             <header className="App-header">
-                <Card style={{ padding: '5rem' }}>
+                <Card style={{ padding: '5rem', width: '75%', margin: '0 auto' }}>
                     <CardContent>
                         <CardMedia
                             component="img"
                             height="140"
-                            image={Libro?.imageLinks?.thumbnail || ''}
+                            image={Libro?.imageLinks?.thumbnail || '/elisa-calvet-b-S3nUOqDmUvc-unsplash.jpg'}
                             alt="Libro sin imagen"
                         />
                         <Box sx={{ mt: 2 }}>
                             <Typography gutterBottom variant="h5" component="div">
                                 {Libro?.volumeInfo?.title}
                             </Typography>
-                            <Typography variant="body2" color="text.secondary">
+                            <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic', fontSize: '20px' }}>
                                 {Libro?.volumeInfo?.authors?.join(', ')}
                             </Typography>
-                            <Typography variant="body2" color="text.secondary">
+                            <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 'bold' , fontSize: '18px'}}>
                                 {Libro?.volumeInfo?.publisher}
                             </Typography>
-                            <Typography variant="body2" color="text.secondary">
+                            <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 'bold', fontSize: '18px'}}>
                                 {Libro?.volumeInfo?.publishedDate}
                             </Typography>
-                            <Typography variant="body2" color="text.secondary">
+                            <Typography variant="body2" color="text.secondary" sx={{ fontSize: '16px' }}>
                                 {Libro?.volumeInfo?.description}
                             </Typography>
                         </Box>
