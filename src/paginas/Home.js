2b4d904006031febddcb4ac626f27528d27adc70
ludelossantos <ludelossantosp@gmail.com>
2023-06-06 22:26:41 -0300

    responsive

--- src/paginas/Home.js
+++ src/paginas/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Header from '../componentes/base/Header';
 import Buscador from '../componentes/listarLibros/Buscador';
 import LibroCard from '../componentes/listarLibros/LibroCard';
@@ -9,17 +9,17 @@ const Home = () => {
     const [books, setBooks] = useState([])
     const [search, setSearch] = useState("")
 
+    const consumeApi = useCallback(async() => {
+        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=' + search.replace(' ', '+'));
+        const responseJson = await response.json();
+        setBooks(responseJson?.items || []);
+    }, [search]);
+
     useEffect(() => {
         if (search !== '') {
-            fetch('https://www.googleapis.com/books/v1/volumes?q=' + search.replace(' ', '+'))
-                .then((response) => {
-                    return response.json()
-                })
-                .then((data) => {
-                    setBooks(data?.items || [])
-                })
+            consumeApi();
         }
-    }, [search])
+    }, [search, consumeApi]);
 
     return (
         <div>
--- src/paginas/InfoLibro.js
+++ src/paginas/InfoLibro.js
@@ -1,59 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Header from '../componentes/base/Header';
-import { Card, CardContent, CardMedia, Typography, Box } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, Box, Grid } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import '../componentes/listarLibros/LibroCard.css'
 
 const InfoLibro = () => {
     const [ Libro, setLibro] = useState({});
     const { titulo } = useParams();
 
-    useEffect(() => {
-        if (titulo !== '') {
-            const formattedTitulo = encodeURIComponent(titulo);
-    
-            fetch('https://www.googleapis.com/books/v1/volumes?q=' + formattedTitulo)
-                .then((response) => {
-                    return response.json()
-                })
-                .then((data) => {
-                    setLibro(data?.items[0] || []);
-                });
-        }
+    const consumeApi = useCallback(async() => {
+        const formattedTitulo = encodeURIComponent(titulo);
+        const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=' + formattedTitulo);
+        const responseJson = await response.json();
+        setLibro(responseJson?.items[0] || []);
     }, [titulo]);
 
-    console.log("libro: ",Libro);
+    useEffect(() => {
+        if (titulo !== '') {
+            consumeApi();
+        }
+    }, [titulo, consumeApi]);
+
+    console.log("imagen: ",Libro?.volumeInfo?.description);
 
     return (
         <div>
             <Header />
             <header className="App-header">
-                <Card style={{ padding: '5rem' }}>
-                    <CardContent>
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            image={Libro?.imageLinks?.thumbnail || ''}
-                            alt="Libro sin imagen"
-                        />
-                        <Box sx={{ mt: 2 }}>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {Libro?.volumeInfo?.title}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {Libro?.volumeInfo?.authors?.join(', ')}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {Libro?.volumeInfo?.publisher}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {Libro?.volumeInfo?.publishedDate}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {Libro?.volumeInfo?.description}
-                            </Typography>
-                        </Box>
-                    </CardContent>
+            <Box sx={{ my: 2 }}>
+                <Card style={{ maxWidth: '1000px', width: '100%' }}>
+                    <Grid container justifyContent="center">
+                        <Grid item xs={12} sm={10} md={8} lg={6}>
+                            <CardMedia
+                                component="img"
+                                height="auto"
+                                image={Libro?.volumeInfo?.imageLinks?.thumbnail || ''}
+                                alt="Libro sin imagen"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={10} md={8} lg={6}>
+                            <CardContent>
+                                <Box sx={{ mt: 2 }}>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {Libro?.volumeInfo?.title}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {Libro?.volumeInfo?.authors?.join(', ')}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {Libro?.volumeInfo?.publisher}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {Libro?.volumeInfo?.publishedDate}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary" style={{ overflow: 'auto', maxHeight: 'auto' }}>
+                                        {Libro?.volumeInfo?.description}
+                                    </Typography>
+                                </Box>
+                            </CardContent>
+                        </Grid>
+                    </Grid>
                 </Card>
+            </Box>
             </header>
         </div>
     );
